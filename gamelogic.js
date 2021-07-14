01fb9579a3baac8cad9379b2f06609cd85472817
ayalena <82506563+ayalena@users.noreply.github.com>
2021-07-14 22:04:45 +0200

    branch

--- gamelogic.js
+++ gamelogic.js
@@ -1,13 +1,67 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  //code to display "_ _ _ _" in accordance to the length of the word
+  // console.log(word);
+  // console.log(guesses);
+
+  // let array = [];
+  //
+  // for (let i = 0; i < word.length; i++) {
+  //   array.push('_ ');
+  //
+  // }
+  //
+  // let text = array.join('').toString();
+  // return text;
+
+  //code to display where the letters that are guessed so far appear on it
+  let output = "";
+  for( let i = 0; i < word.length; i++) {
+    if (guesses.includes(word[i])) {
+      output = output + word[i] + " ";
+    } else {
+      output = output + "_ ";
+    }
+  }
+  return output;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  let output = displayWordSoFar(word, guesses);
+  output = output.split(" ").join("");
+  console.log(output);
+
+  if (word === output) {
+    return true;
+  } else {
+    return false;
+  }
+  // return word === output;
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  //maak een array voor foute letters in op te slaan
+  let wrongLetters = [];
+  // guesses = [];
+  for (let i = 0; i < guesses.length; i++) {
+    if (guesses[i] !== word) {
+      wrongLetters.push(guesses[i]);
+    } else {
+      guesses.push(guesses[i]);
+    }
+  }
+
+  if (wrongLetters.length >= 7) {
+    return true;
+  } else if (wrongLetters.length < 7) {
+    return false;
+  }
+
+  //if you've guessed 7 wrong letters or more or less than 7 letters
+  // if (guesses.length >= 7) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,14 +2,52 @@ const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
 function game(word, guesses) {
+  //Zorg dat de gebruiker letters kan raden
   console.log("Dit heb je tot nu toe geraden: ", guesses);
+  let letter = question("Raad een letter: ");
 
-  const letter = question("Raad een letter: ");
 
-  // voeg de geraden letter toe aan de array met guesses
-  guesses.push(letter);
+  //Houdt in de guesses-array bij welke letters de gebruiker geraden heeft
+  if (word.includes(letter)) {
+    guesses.push(letter);
+  }
+
+  //array voor foute letters?
+  // let wrongLetters = [];
+  // for (let i = 0; i < word.length; i++) {
+  //   if (word.includes(letter)) {
+  //     guesses.push(letter);
+  //   } else {
+  //     wrongLetters.push(letter);
+  //   }
+  // }
+
+  //Laat zien welke letters al geraden zijn (met displayWordSoFar)
+  displayWordSoFar(word, guesses);
+
+
+  //Heeft iemand alle letters geraden? Wuhuuu! Log dan dat ze gewonnen hebben
+  if (isGameWon(word, guesses)) {
+    console.log("You won!");
+  }
+
+  //Heeft iemand 7 letters fout geraden? Jammer! Log dan dat ze verloren hebben
+  if (isGameLost(word, guesses)) {
+    console.log("Ya lose sucker");
+    return;
+  }
+
+  //Stop het programma met return wanneer iemand gewonnen of verloren heeft
+  if (isGameLost(word, guesses) || isGameWon(word, guesses)) {
+    return;
+  }
+  // } else {
+  //   game(word, guesses);
+  // }
+  // Heeft iemand nog niet gewonnen of verloren? Dan gaat het spel gewoon verder
 
   // volgende ronde! we roepen game nog een keer aan
+
   game(word, guesses);
 }
 
